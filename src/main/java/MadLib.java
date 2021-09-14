cff32b84269524cdb719d5b4f2c625642d0828ab
kcorad4 <59891211+kcorad4@users.noreply.github.com>
2021-09-13 20:44:37 -0400

    Add files via upload

---
+++ src/main/java/MadLib.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MadLib {
+
+    public static void main(String[]args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a noun:");
+        String noun = sc.nextLine();
+        System.out.println("enter a verb");
+        String verb = sc.nextLine();
+        System.out.println("enter an adjective");
+        String adjective = sc.nextLine();
+        System.out.println("enter an adverb");
+        String adverb = sc.nextLine();
+
+        System.out.println("Do you " + verb + " your " +adjective + " " +noun +" " +adverb + "? That's hilarious!");
+
+    }
+}
